eef046940506f7330a3350f708b786928b5a66df
Hugolebl <hugo.leblanc53@gmail.com>
2025-08-27 13:58:37 +0100

    new commit

--- R/get_correct_id.R
+++ R/get_correct_id.R
@@ -25,6 +25,10 @@ get_correct_id <- function(new_shp, old_shp, crs){
    sf::st_geometry(old_shp) <- "geometry"
    sf::st_geometry(new_shp) <- "geometry"
 
+   # Remove empty polygons
+   new_shp <- new_shp %>%
+      filter(!st_is_empty(geometry))
+
    # Create new variables
    new_shp$idmodif_idn <- NA
    new_shp$idmodif_geom <- NA
@@ -52,25 +56,42 @@ get_correct_id <- function(new_shp, old_shp, crs){
       dist <- sf::st_centroid(new_shp[i,'geometry']) %>%
          sf::st_distance( sf::st_centroid(old_shp$geometry)) %>%
          as.numeric()
-      if(length(which(dist == 0)) == 1){same_geom <- old_shp$id[which(dist == 0)]}else{same_geom <- NULL}
 
-      if(!is.null(same_geom)){
+      if(length(which(dist == 0)) == 1){
 
-         if(!new_shp$id[i] == same_geom){
+         same_geom <- old_shp$id[which(dist == 0)]
+
+         if(new_shp$id[i] == same_geom) {
+            new_shp$idmodif_geom[i] = 'NON'
+         }
+
+         if(new_shp$id[i] != same_geom) {
             new_shp$id[i] = same_geom
             new_shp$idmodif_geom[i] = 'OUI'
          }
 
       }
 
+      if(length(which(dist == 0)) == 0){
+         new_shp$idmodif_geom[i] = 'no match found'
+      }
+
+      if(length(which(dist == 0)) > 1){
+         new_shp$idmodif_geom[i] = 'more than 1 match found'
+      }
+
    }
 
    correct_id_by_idn <- new_shp %>% filter(idmodif_idn == 'OUI') %>% .[['id']]
    correct_id_by_geom <- new_shp %>% filter(idmodif_geom == 'OUI') %>% .[['id']]
+   no_match_found_by_geom <- new_shp %>% filter(idmodif_geom == 'no match found') %>% .[['id']]
+   several_match_found_by_geom <- new_shp %>% filter(idmodif_geom == 'more than 1 match found') %>% .[['id']]
 
    mylist <- list(corrected_shp = new_shp,
                   correct_id_by_idn = correct_id_by_idn,
-                  correct_id_by_geom = correct_id_by_geom)
+                  correct_id_by_geom = correct_id_by_geom,
+                  no_match_found_by_geom = no_match_found_by_geom,
+                  several_match_found_by_geom = several_match_found_by_geom)
 
    return(mylist)
 
